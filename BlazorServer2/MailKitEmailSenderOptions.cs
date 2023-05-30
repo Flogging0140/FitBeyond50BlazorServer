f864f2a8b24947614a5e0f4e557f8f69d6935006
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2023-05-30 08:03:43 -0600

    Added enum to tell what type of email they are sending, added other methods to get and customize html to be a bit different. Changed up html file so I have txt to look for, removed old one.

--- BlazorServer2/MailKitEmailSenderOptions.cs
+++ BlazorServer2/MailKitEmailSenderOptions.cs
@@ -19,15 +19,27 @@ namespace BlazorServer2
 
         public MailKitEmailSenderOptions Options { get; set; }
 
-        public Task SendEmailAsync(string email, string subject, string message)
+        public enum EmailType
         {
-            return Execute(email, subject, message);
+            subscribe,
+            unsubscribe,
+            sharePost
         }
 
-        public Task Execute(string to, string subject, string message)
+        public Task SendEmailAsync(string email, string subject, string message, EmailType emailType)
         {
-            if (string.IsNullOrEmpty(message))
-                message = GetTemplateHtml();
+            return Execute(email, subject, message, emailType);
+        }
+
+        public Task Execute(string to, string subject, string message, EmailType emailType)
+        {
+            // check type
+            if (emailType == EmailType.subscribe)
+                message = GetSubscribedHtml();
+            else if (emailType == EmailType.unsubscribe)
+                message = GetUnSubscribedHtml();
+            else if (emailType == EmailType.sharePost)
+                message = GetSharedPosHtml(message);
 
             // create message
             var email = new MimeMessage();
@@ -51,14 +63,48 @@ namespace BlazorServer2
             return Task.FromResult(true);
         }
 
-        private string GetTemplateHtml()
+        // get html to show shared post, customize the same html
+        private string GetSharedPosHtml(string postHtmlContent)
         {
-            //var owners = System.IO.File.ReadAllLines(@"..\data\Owners.txt");
             string pathRoot = _enviroment.ContentRootPath;
             string fullPath = System.IO.Path.Combine(pathRoot, @"Data\HtmlSubscribedTemplate.html");
             var owners = System.IO.File.ReadAllLines(fullPath);
-            
+
             string retMessage = string.Join(Environment.NewLine, owners);
+
+            // replace "TEXT_HERE" with message we want
+            retMessage = retMessage.Replace("TEXT_HERE", postHtmlContent);
+
+            return retMessage;
+        }
+
+        // html to show this (TODO: should probe select unsubscribed in db (bit value))
+        private string GetUnSubscribedHtml()
+        {
+            string pathRoot = _enviroment.ContentRootPath;
+            string fullPath = System.IO.Path.Combine(pathRoot, @"Data\HtmlSubscribedTemplate.html");
+            var owners = System.IO.File.ReadAllLines(fullPath);
+
+            string retMessage = string.Join(Environment.NewLine, owners);
+
+            // replace "TEXT_HERE" with message we want
+            retMessage = retMessage.Replace("TEXT_HERE", "You've been unsubscribed; hope to meet again soon!");
+
+            return retMessage;
+        }
+
+        // get html for subscribed person (TODO: add their email to database in new table)
+        private string GetSubscribedHtml()
+        {
+            string pathRoot = _enviroment.ContentRootPath;
+            string fullPath = System.IO.Path.Combine(pathRoot, @"Data\HtmlSubscribedTemplate.html");
+            var owners = System.IO.File.ReadAllLines(fullPath);
+
+            string retMessage = string.Join(Environment.NewLine, owners);
+
+            // replace "TEXT_HERE" with message we want
+            retMessage = retMessage.Replace("TEXT_HERE", "You have Subscribed! 🥳");
+
             return retMessage;
         }
     }
