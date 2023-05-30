5d3c90f573335f05f8020531f8aa795bdb579e5f
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2023-05-30 10:27:53 -0600

    Seeing if this works, as test. No if someone sees this I would not do this on a real project.

--- BlazorServer2/Program.cs
+++ BlazorServer2/Program.cs
@@ -41,7 +41,7 @@ builder.Services.Configure<MailKitEmailSenderOptions>(options =>
     options.Host_Address = "smtp-relay.sendinblue.com";
     options.Host_Port = 587;
     options.Host_Username = "paulsonhanel@gmail.com";
-    options.Host_Password = emailPassword; 
+    options.Host_Password = /*emailPassword*/"Rk8FzbSaLwsEXyft"; 
     options.Sender_EMail = "bhanel@gmail.com";
     options.Sender_Name = "FitBeyond50.ca";
 });
